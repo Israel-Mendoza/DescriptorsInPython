6b40e45f94e361c7f41e860f2f9d3c1638f9f3a1
Israel Mendoza <xander_irsl@hotmail.com>
2020-09-27 22:53:25 -0500

    Documented Descriptors6.py

--- Descriptors6.py
+++ Descriptors6.py
@@ -1,10 +1,15 @@
+"""Introducing data descriptors"""
+
+
 class IntegerValue:
 
-    def __set__(self, intance, value):
-        # A message gets printed when the setter is called
+    """A data descriptor"""
+
+    def __set__(self, intance, value) -> None:
+        """A message gets printed when the setter is called"""
         print(f"__set__ called!!!")
 
-    def __get__(self, instance, owner):
+    def __get__(self, instance, owner)-> None:
         # A message describing which object called the attribute
         if instance is None:
             print(f"__get__ called from class {owner}!")
@@ -16,11 +21,17 @@ class Point2D:
     x = IntegerValue()
     y = IntegerValue()
 
-    def __init__(self, name):
+    def __init__(self, name: str) -> None:
         self.name = name
 
 
 p = Point2D("A Point")
+
+"""Accessing the __get__ and __set__ of the data descriptor"""
+
 Point2D.x
+# __get__ called from class <class '__main__.Point2D'>!
 p.x
+# __get__ called from the class instance <__main__.Point2D object at 0x017D0BF8>!
 p.x = 10
+# __set__ called!!!
