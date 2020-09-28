3326ea1e826e95361eb0c697b087332e87ecdc42
Israel Mendoza <xander_irsl@hotmail.com>
2020-09-27 22:28:48 -0500

    Documented Descriptors4.py

--- Descriptors4.py
+++ Descriptors4.py
@@ -1,13 +1,22 @@
+"""
+Non-data descriptors.
+We can change the behavior of the descriptor whether 
+it is accessed from a class or an instance.
+"""
+
+
 from datetime import datetime
 
 
 class UTCTime:
 
+    """Non-data descriptor."""
+
     def __get__(self, instance, owner):
         """
         Returns the descriptor instance if called from a class.
         Returns the current UTC datetime in ISO format if called
-            from a class instance.
+        from a class instance.
         """
         if instance is None:
             return self
@@ -16,11 +25,20 @@ class UTCTime:
 
 
 class Logger:
+    """A simple logger class"""
 
     current_time = UTCTime()
 
 
+# Instantiating the Logger
 logger = Logger()
 
-print(type(Logger.current_time))
-print(type(logger.current_time))
+"""Accessing the descriptor"""
+
+# Expecting the descriptor's instance
+print(Logger.current_time)
+# <__main__.UTCTime object at 0x010BB808>
+
+# Expecting the time string
+print(logger.current_time)
+# 2020-09-28T03:26:29.654123
